ec4a374ab06516d243dc218ab917285c17c0a4e4
Nexus-Strife <34130887+Nexus-Strife@users.noreply.github.com>
2019-01-18 16:03:45 +0100

    Update mk_table.py

--- web_app/management/commands/mk_table.py
+++ web_app/management/commands/mk_table.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
         cmds = [
             """CREATE TABLE IF NOT EXISTS web_app_categories (id SERIAL PRIMARY KEY, name VARCHAR(50))""",
             """CREATE TABLE IF NOT EXISTS web_app_products(id SERIAL PRIMARY KEY, name VARCHAR(255), store VARCHAR(255), grade CHAR(1), kj_100g INTEGER, cat_id_id INTEGER, foreign key (cat_id_id) REFERENCES web_app_categories(id), url VARCHAR(255), description TEXT, img_url VARCHAR(255), proteins_100g REAL, sugars_100g REAL, fat_100g REAL, salt_100g REAL, saturated_fat_100g REAL)""",
-            """CREATE TABLE IF NOT EXISTS web_app_favs(id SERIAL PRIMARY KEY, prod_id INTEGER NOT NULL, UNIQUE (prod_id), foreign key (prod_id) REFERENCES web_app_products(id), prod_substitute_id INTEGER, foreign key (prod_substitute_id) REFERENCES web_app_products(id), user_id INTEGER NOT NULL, foreign key (user_id) REFERENCES auth_user(id))"""]
+            """CREATE TABLE IF NOT EXISTS web_app_favs(id SERIAL PRIMARY KEY, prod_id INTEGER NOT NULL, foreign key (prod_id) REFERENCES web_app_products(id), prod_substitute_id INTEGER, foreign key (prod_substitute_id) REFERENCES web_app_products(id), user_id INTEGER NOT NULL, foreign key (user_id) REFERENCES auth_user(id))"""]
 
         conn = None
 
