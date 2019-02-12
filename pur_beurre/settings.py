b6ab1add293ef50ca6bd6cc800b4adedc9945c52
Nexus-Strife <34130887+Nexus-Strife@users.noreply.github.com>
2019-02-12 18:49:26 +0100

    Update settings.py

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2sgeo63##%r81l9d605jlnaw+x46+_@g4mv@q-#(4q6o6jhq%z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
